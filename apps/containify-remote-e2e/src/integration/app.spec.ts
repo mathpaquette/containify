05967ec6c77efedffc447be6dd63cddec9c1a894
Mathieu Paquette <me@mathpaquette.com>
2021-10-04 21:51:25 -0400

    ci(containify-host-e2e): fix e2e script

--- apps/containify-remote-e2e/src/integration/app.spec.ts
+++ apps/containify-remote-e2e/src/integration/app.spec.ts
@@ -1,13 +1,7 @@
-import { getGreeting } from '../support/app.po';
-
 describe('containify-remote', () => {
   beforeEach(() => cy.visit('/'));
 
-  it.skip('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to containify-remote!');
+  it('should display display home', () => {
+    cy.log('home works!');
   });
 });
