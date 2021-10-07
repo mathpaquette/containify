a8af1e731255ca390813f77f282bab962fd12b33
Mathieu Paquette <me@mathpaquette.com>
2021-10-07 07:33:17 -0400

    refactor: remove postmate references

--- apps/containify-remote/src/app/app.module.ts
+++ apps/containify-remote/src/app/app.module.ts
@@ -4,9 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
-/* eslint @typescript-eslint/ban-ts-comment: off */
-// @ts-ignore
-import { Postmate } from 'postmate/src/postmate';
 import { Route1Component } from './route1/route1.component';
 import { Route2Component } from './route2/route2.component';
 import { Route3Component } from './route3/route3.component';
